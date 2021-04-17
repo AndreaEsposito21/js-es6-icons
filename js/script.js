e0feb0ddd02658f508e5c2d2b951fa22294b3d96
Andrea Esposito <andexpox@gmail.com>
2021-04-17 19:46:13 +0200

    wip

--- js/script.js
+++ js/script.js
@@ -112,3 +112,80 @@ const colors = [
 
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+const iconsContainer = $('#icons-container');
+const arraycolored = colorIcons(icons, colors);
+printIcons(arraycolored, iconsContainer);
+
+/* ------------ FUNZIONI ------------ */
+
+// Popola il container con le icone
+//
+// array icone --> array di oggetti dove ogni oggetto è un'icona
+// container --> un oggetto jQuery che rappresenta l'elemento in cui si stamperanno le icone
+//
+// retur: vuoto
+function printIcons(arrayIcone, container) {
+	// ForEach per scrivere le icone nel container
+	arrayIcone.forEach(element =>  {
+
+		// Destructuring
+		const {name, prefix, family, color} = element;
+		let nomeMaiuscolo = name.toUpperCase();
+
+		const iconElementHTML = `
+		<div class="icon">
+            <i class="${family} ${prefix}${name}" style="color: ${color};"></i>
+            <div>
+                ${nomeMaiuscolo}
+            </div>
+        </div>
+		`;
+
+		container.append(iconElementHTML);
+	});
+}
+
+// Nuovo array di icone con colori
+//
+// arrayIconeOriginali --> array di oggetti che rappresentano le icone
+// colorArray --> array di stringhe che rappresentano un colore css
+//
+// return: array di oggetti in cui ogni oggitti rappresenta un'icona con anche i colori
+function colorIcons(arrayIconeOriginali, colorArray) {
+	const iconsType = getIconsType(arrayIconeOriginali);
+
+	const iconeConColori = arrayIconeOriginali.map((element) => {
+		const newIconOBj = {
+			...element
+		};
+
+		const iconTypeIndex = iconsType.indexOf(newIconOBj.type);
+
+		if (iconTypeIndex != -1) {
+			newIconOBj.color = colorArray[iconTypeIndex];
+		};
+
+		return newIconOBj;
+	});
+
+	return iconeConColori;
+};
+
+// Array con type di icons
+//
+// iconsArray --> array di oggetti che rappresentano le icone
+//
+// return: array di stringhe
+function getIconsType(iconsArray) {
+	const typeArray = [];
+
+	iconsArray.forEach( (element) => {
+		const elementType = element.type;
+
+		if ( typeArray.includes(elementType) == false ) {
+			typeArray.push(elementType);
+		}
+	});
+
+	return typeArray;
+};
